e1c79853e85da3d3ec035668a06467577c206250
Johan G <jojo8510@gmail.com>
2016-01-19 07:37:44 +0100

    prouf of no return pointeur tab

--- fillit/test.c
+++ fillit/test.c
@@ -16,3 +16,25 @@ int	test(void)
 {
 	return (error());
 }
+
+void test3(char **tab)
+{
+	tab[1] = "slt\0";
+}
+void test2(void)
+{
+	char **tab;
+	int i;
+	
+	i = 0;	
+	tab = (char **)malloc(sizeof(char *) * 3);
+	tab[2] = NULL;
+	while (i < 2)
+		tab[i++] = (char *)malloc(sizeof(char) * 4);
+	
+	tab[0] = "bjr\0";
+	test3(tab);
+	printf("tab[0] = %s\n", tab[0]); //bjr
+	printf("tab[1] = %s\n", tab[1]); //slt
+	printf("tab[2] = %s\n", tab[2]); //null
+}
