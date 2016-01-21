f18a0ed2e0232cb30584f1a5815bf91b6fd09b3a
Johan GIRAUDEAU <jgiraude@e2r8p17.42.fr>
2016-01-21 14:42:40 +0100

    update verifsquare

--- fillit/verifsquare.c
+++ fillit/verifsquare.c
@@ -6,7 +6,7 @@
 /*   By: jgiraude <jgiraude@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/20 19:22:40 by jgiraude          #+#    #+#             */
-/*   Updated: 2016/01/20 20:45:10 by jgiraude         ###   ########.fr       */
+/*   Updated: 2016/01/21 14:42:10 by jgiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,24 @@
 int		verifsquare(char **square, int taille, char *tab, int coord[1])
 {
 	int i;
-	int j;
 	int x;
 	int y;
 
 	i = 0;
-	j = 0;
 	x = coord[0];
 	y = coord[1];
-	while (i < 15) //pour tout tab
-	{
-		if ((y > taille) && (x > taille))
+	if ((y >= taille) && (x >= taille))
 			return (1); //pas possible
+	while (i < 15) //pour tout tab
+	{*
 		if (i % 4 == 0) //pour changer de dimension dans square
 		{
 			y++;
-			x = coord[0]; //reinitialise j au coords de depart.
+			x = coord[0]; //reinitialise x au coords de depart.
 		}
 		if (tab[i] == '#')
 		{
-			if (x + (i % 4) > taille)
+			if ((x + (i % 4) > taille) || (y + (i % 4) > taille)) //a verif, pas sur avec y
 				return (2); //supperieur a taille
 			if (square[y][x + (i % 4)] == '#')
 				return (3); //supperposition
