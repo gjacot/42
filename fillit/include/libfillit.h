a170c034dab2dc58091314fc3cb149776e4d587f
Johan GIRAUDEAU <jgiraude@e2r8p21.42.fr>
2016-01-27 16:05:54 +0100

    update makefile + proto

--- fillit/include/libfillit.h
+++ fillit/include/libfillit.h
@@ -6,7 +6,7 @@
 /*   By: jgiraude <jgiraude@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/10 15:22:16 by gjacot            #+#    #+#             */
-/*   Updated: 2016/01/21 18:15:15 by jgiraude         ###   ########.fr       */
+/*   Updated: 2016/01/27 16:02:37 by jgiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,39 +31,30 @@ typedef	struct		s_piece
 
 int					error(void);
 int					error2(char *str);
-int					test(void);
+void				affiche_lst(t_piece *lst);
+void				clearsquare(char **piece, int taille);
+int					isnextto(int *die, int j, int *line);
+int					countline(int *die);
 int					lastcheck(char *buf, int i);
-int					solveit(char *buf);
-char				**inittab(char *buf);
-void				**remplir_tab(char *buf, char **tab_piece);
-void				affiche(char **tab_piece);
-int					compte_tab(char *buf);
-char				**initsquare(int square);
+int					checkerror(char *buf, int i, int line, int col);
+void				nextsquare(int *col, int *line, int *dies, int *i);
+int					filecheck(char *buf, int i, int line);
+void				moveit(t_piece piece, int taille, char *tab, char **square);
+t_piece				*newpiece(char lettre);
 int					verif_collone(char *tab, int num);
 int					verif_ligne(char *tab, int num);
 void				update_ligne(char *tab, char lettre);
 void				update_collone(char *tab, char lettre);
 void				posi(char *tab, int lettre);
-void				affiche_lst(t_piece *lst);
 t_piece				*remplir_list(char **tab_piece, t_piece *lst);
-t_piece				*newpiece(char lettre);
+int					firstsquare(int i);
+void				affiche(char **tab_piece);
+int					solveit(char *buf);
+int					compte_tab(char *buf);
+void				**remplir_tab(char *buf, char **tab_piece);
+char				**inittab(char *buf);
+char				**initsquare(int square);
+int					verifsquare(char **square, int taille,
+					char *tab, int coord[1]);
 
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
