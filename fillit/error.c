74fd0ce49c83803275f006eaef0cf045c69747cc
Guillaume JACOT <gjacot@e2r4p18.42.fr>
2016-01-29 20:36:04 +0100

    error fd

--- fillit/error.c
+++ fillit/error.c
@@ -6,21 +6,21 @@
 /*   By: jgiraude <jgiraude@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/10 15:30:46 by gjacot            #+#    #+#             */
-/*   Updated: 2016/01/18 15:45:39 by jgiraude         ###   ########.fr       */
+/*   Updated: 2016/01/29 17:37:50 by gjacot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include <stdlib.h>
 
-void	error(void)
+void	error()
 {
 	ft_putstr("error\n");
 	exit(0);
 }
 
-void	error2(char *str)
+void	error2(char *str, int fd)
 {
-	ft_putstr(str);
+	ft_putstr_fd(str, fd);
 	exit(0);
 }
