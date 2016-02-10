cc198fc4d430d9fc14695b91f2b84ce7c672c2b3
Guillaume JACOT <gjacot@e2r5p18.42.fr>
2016-02-10 14:45:33 +0100

    error norme

--- fillit/error.c
+++ fillit/error.c
@@ -6,14 +6,14 @@
 /*   By: jgiraude <jgiraude@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/10 15:30:46 by gjacot            #+#    #+#             */
-/*   Updated: 2016/01/29 17:37:50 by gjacot           ###   ########.fr       */
+/*   Updated: 2016/02/10 14:45:04 by gjacot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include <stdlib.h>
 
-void	error()
+void	error(void)
 {
 	ft_putstr("error\n");
 	exit(0);
