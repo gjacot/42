69ae498e2ad47b688ebddc861f85de45259944e7
Guillaume JACOT <gjacot@e2r5p18.42.fr>
2016-02-10 15:58:10 +0100

    verifsquare pseudonorme

--- fillit/verifsquare.c
+++ fillit/verifsquare.c
@@ -6,43 +6,37 @@
 /*   By: jgiraude <jgiraude@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/20 19:22:40 by jgiraude          #+#    #+#             */
-/*   Updated: 2016/01/21 15:52:37 by jgiraude         ###   ########.fr       */
+/*   Updated: 2016/02/10 15:33:31 by gjacot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- 
 #include <libfillit.h>
 #include <libft.h>
 
 int		verifsquare(char **square, t_piece *piece)
 {
-	printf("--- VERIFSQUARE DEBUT ---\n");
 	int i;
 	int x;
 	int y;
 	int len;
 	int compteur;
 
+	printf("--- VERIFSQUARE DEBUT ---\n");
 	compteur = 0;
 	len = ft_strlen(square[0]);
 	i = 0;
-	
 	x = piece->x;
 	y = piece->y;
 	y--;
 	printf("verifsquare - len = %d\n", len);
-
 	printf("verifsquare - piece = %s\n", piece->piece);
-
 	printf("verifsquare - BOUCLE\n\n\n");
-	
 	while (i < 15) //pour tout tab
 	{
-	printf("verifsquare - i = %d\n", i);
-	printf("verifsquare - x = %d\n", x);
-	printf("verifsquare - y = %d\n", y);
-	
-		if ( i % 4 == 0 && compteur < 4)//pour changer de dimension dans square car square = char** et piece->piece = char *
+		printf("verifsquare - i = %d\n", i);
+		printf("verifsquare - x = %d\n", x);
+		printf("verifsquare - y = %d\n", y);
+		if (i % 4 == 0 && compteur < 4)//pour changer de dimension dans square car square = char** et piece->piece = char *
 		{
 			printf("verifsquare - DEBUT 1er if\n");
 			x = piece->x; //reinitialise x au coords de depart.
@@ -51,31 +45,25 @@ int		verifsquare(char **square, t_piece *piece)
 			printf("verifsquare - y = %d\n", y);
 			printf("verifsquare - FIN 1er if\n");
 		}
-
-		
 		if (x >= len && piece->piece[i] != '.')
 		{
 			printf("verifsquare - x >= len \n");
 			return (4);
 		}
-
-		if (y >= len) //y est sortie du tableau (sous entend qu'on a tester tout la piece et qu'elle ne rentre pas) 
+		if (y >= len) //y est sortie du tableau (sous entend qu'on a tester tout la piece et qu'elle ne rentre pas)
 		{				//oublie du = car len commance a 1 et y a 0
 			printf("verifsquare - y >= len \n");
 			return (2); //la piece ne rentre pas, on passe a la piece precedante
 		}
-		
-			
-
-		if (piece->piece[i] != '.') //ne s'occupe que des pieces deja presente 
+		if (piece->piece[i] != '.') //ne s'occupe que des pieces deja presente
 		{
 			printf("verifsquare - DEBUT 3er if\n");
 			printf("verifsquare - x = %d\n", x);
 			printf("verifsquare - y = %d\n", y);
 			if (square[y][x] != '.') //c'est cette PUTAIN de ligne qui fait chier car il test y = 3 et x = 0
 			{
-			printf("verifsquare - x2 = %d\n", x);
-			printf("verifsquare - y2 = %d\n", y);
+				printf("verifsquare - x2 = %d\n", x);
+				printf("verifsquare - y2 = %d\n", y);
 				return (3); //supperposition
 			}
 			printf("verifsquare - FIN 3er if\n");
@@ -84,16 +72,16 @@ int		verifsquare(char **square, t_piece *piece)
 		{
 			compteur++;
 			printf("verifsquare - compteur = %d\n", compteur);
-			if(compteur == 4)
+			if (compteur == 4)
 			{
 				printf("verifsquare - compteur = %d\n RETURN (0)", compteur);
-				return(0);
+				return (0);
 			}
 		}
 		i++;
 		x++;
-			printf("verifsquare - x = %d\n", x);
-	printf("verifsquare - y = %d\n", y);
+		printf("verifsquare - x = %d\n", x);
+		printf("verifsquare - y = %d\n", y);
 		printf("verifsquare - BOUCLE2\n\n");
 	}
 	//on peu placer la piece ici sinon
