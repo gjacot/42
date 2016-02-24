531903f3871c0d5cc3536af42029c61f72a2da3a
Guillaume JACOT <gjacot@e2r7p18.42.fr>
2016-02-24 17:56:30 +0100

    moveitulti

---
+++ fillit/moveitulti.c
@@ -0,0 +1,153 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   moveit.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgiraude <jgiraude@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/17 16:40:00 by gjacot            #+#    #+#             */
+/*   Updated: 2016/02/24 17:24:30 by gjacot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+#include <libfillit.h>
+
+char	**placeit(char **square, t_piece *piece, int len)
+{
+	int x;
+	int y;
+	int dies;
+	int i;
+
+	printf("--------placeit--------\n");
+	x = piece->x;
+	y = piece->y;
+	dies = 0;
+	i = 0;
+	affiche(square);
+	printf("len = %d\n", len);
+	while (dies != 4)
+	{
+		printf("x = %d\ny = %d\ni = %d\npiece[i] = %c\n", x, y, i, piece->piece[i]);
+		if (piece->piece[i] != '.')
+		{
+			printf("----------------------------\n");
+			square[y][x] = piece->piece[i];
+			affiche(square);
+			dies++;
+		}
+		if (x >= len - 1)
+		{
+			x = 0;
+			y++;
+		}
+		else
+			x++;
+		i++;
+	}
+	return (square);
+}
+
+int		ft_verifpieceactu(char **square, char lettre, int len)
+{
+	int i;
+	int j;
+	int fail;
+
+	i = 0;
+	j = 0;
+	fail = 0;
+	while (dies != 4 && dies2 != 4 && i < 4)
+	{
+		printf("----------\ni=%d\nj=%d\n", i, j);
+		if (j >= len)
+		{
+			i++;
+			j = 0;
+		}
+		else if (square[i][j] == lettre)
+		{
+			if (square[i][j++] != lettre || square[i][j++] != '.' ||
+				square[i++][j] != lettre || square[i++][j] != '.')
+				fail = 1;
+		}
+		else
+			j++;
+	}
+	return ()
+}
+
+void	ft_pieceprev(char **square, t_piece *piece, int len)
+{
+	int verif;
+
+	printf("--------ft_pieceprev--------\n");
+	printf("PIECE ACTU->%c\n",piece->lettre);
+	verif = ft_verifpieceactu(square, piece->lettre, len);
+	printf("verif=%d\n", verif);
+	if (verif == 0)
+	{
+		piece->x = 0;
+		piece->y = 0;
+		piece = piece->prev;
+		piece->x++;
+		printf ("piece->y = %d\n", piece->y);
+		if (piece->x > len)
+		{
+			piece->x = 0;
+			piece->y++;
+		}
+		clearsquare(square, piece->lettre, len);
+		printf("PIECE ACTU->%c\n",piece->lettre);
+	}
+	clearsquare(square, piece->lettre, len);
+	if (verif == 1)
+		piece->x++;
+	else
+		piece->y++;
+}
+
+char	**moveit(t_piece *piece, char **square)
+{
+	int verif;
+	int len;
+	int ok;
+
+	printf("--------moveit--------\n");
+	ok = 0;
+	len = ft_strlen(square[0]);
+	while (ok == 0)
+	{
+		verif = verifsquare(square, piece);
+		if (verif == 0)
+		{
+			square = placeit(square, piece, len);
+			if (piece->next != NULL)
+				piece = piece->next;
+			else
+				ok = 1;
+		}
+		else if (verif != 0 && verif != 2)
+		{
+			if (piece->x > len)
+			{
+				piece->x = 0;
+				piece->y++;
+			}
+			else
+				piece->x++;
+		}
+		else if (verif == 2 && piece->prev == NULL)
+		{
+			len++;
+			clearsquare(square, piece->lettre, len);
+		}
+		else if (verif == 2 && piece->prev != NULL)
+		{
+			ft_pieceprev(square, piece, len);
+			piece = piece->prev;
+		}
+	}
+	return (square);
+}
