cd6c98edeecfe654466aaff33bb9ce602eb1d8e2
Guillaume JACOT <gjacot@e2r8p16.42.fr>
2016-04-06 16:04:18 +0200

    get_next_line almost

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -5,87 +5,54 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gjacot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/03/11 17:41:27 by gjacot            #+#    #+#             */
-/*   Updated: 2016/04/01 17:59:32 by gjacot           ###   ########.fr       */
+/*   Created: 2016/04/05 15:44:58 by gjacot            #+#    #+#             */
+/*   Updated: 2016/04/06 16:03:31 by gjacot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_next_line(char *tmp)
+int	get_the_following(t_get *get, char *str, char **line, int ret)
 {
-	int i;
-	int len;
+	int	i;
 
-	len = ft_strlen(tmp);
 	i = 0;
-	while (tmp[i] != '\n' && i < len)
+	while (str[i - 1] != '\n')
 		i++;
-	return (i);
-}
-
-void	lastcheck(char **line)
-{
-	int		len;
-	char	*n;
-	int		i;
-
-	i = 0;
-	n = ft_strchr(*line, '\n');
-	len = ft_strlen(*line);
-	if (n != NULL)
+	if (i <= (int)ft_strlen(str))
 	{
-		n = ft_strsub(*line, 0, len);
-		free(*line);
-		while (n[i] != '\n')
-			i++;
-		*line = ft_strsub(n, 0, i);
+		get->tmp = ft_strsub(str, i, ft_strlen(str));
+		*line = ft_strsub(str, 0, i - 1);
+		str = NULL;
 	}
+	if (ret < BUFF_SIZE)
+		return (0);
+	return (1);
 }
 
-void	save_tmp(char *tmp)
+int	get_next_line(const int fd, char **line)
 {
-	int		i;
-	int		len;
+	static t_get	get;
+	int				ret;
+	char			*str;
 
-	i = 0;
-	len = ft_strlen(tmp);
-	while (tmp[i - 1] != '\n')
-		i++;
-	tmp = ft_strsub(tmp, i, len);
-}
-
-void	tmp_check(char **tmp, char **line)
-{
-	*tmp = ft_strnew(BUFF_SIZE);
-	*line = NULL;
-}
-
-int		get_next_line(const int fd, char **line)
-{
-	int			i;
-	int			ret;
-	static char	*tmp;
-
-	ret = 0;
-	i = 0;
 	if (BUFF_SIZE <= 0 || fd < 0)
 		return (-1);
-	if (tmp == NULL)
-		tmp_check(&tmp, line);
+	if (!get.tmp)
+		get.tmp = ft_strnew(BUFF_SIZE);
 	else
-		*line = ft_strsub(tmp, 0, ft_strlen(tmp));
-	while (ft_strchr(tmp, '\n') == NULL || ft_strcmp(tmp, *line) == 0)
 	{
-		i = 0;
-		ret = read(fd, tmp, BUFF_SIZE);
-		i = get_next_next_line(tmp);
-		if (*line != NULL)
-			*line = ft_strjoin(*line, tmp);
-		else
-			*line = ft_strsub(tmp, 0, i);
+		str = ft_strsub(get.tmp, 0, ft_strlen(get.tmp));
+		ft_strclr(get.tmp);
 	}
-	save_tmp(tmp);
-	lastcheck(line);
-	return (0);
+	while (ft_strchr(get.tmp, '\n') == NULL && ret != 0)
+	{
+		ret = read(fd, get.tmp, BUFF_SIZE);
+		printf("tmp=%s\n", get.tmp);
+		if (str == NULL)
+			str = ft_strsub(get.tmp, 0, ft_strlen(get.tmp));
+		else
+			str = ft_strjoin(str, get.tmp);
+	}
+	return (get_the_following(&get, str, line, ret));
 }
